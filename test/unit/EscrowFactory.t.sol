011fa5050b6d329ae6d786692937c0f27029f2ad
Mikhail Melnik <by.zumzoom@gmail.com>
2024-02-08 14:24:32 +0400

    fix

--- test/unit/EscrowFactory.t.sol
+++ test/unit/EscrowFactory.t.sol
@@ -9,7 +9,6 @@ import { PackedAddresses, PackedAddressesMemLib } from "../utils/libraries/Packe
 import { Timelocks, TimelocksLib } from "contracts/libraries/TimelocksLib.sol";
 
 import { Address, AddressLib, BaseSetup, IOrderMixin } from "../utils/BaseSetup.sol";
-import "forge-std/console.sol";
 
 contract EscrowFactoryTest is BaseSetup {
     using AddressLib for Address;
@@ -57,11 +56,11 @@ contract EscrowFactoryTest is BaseSetup {
         assertEq(returnedImmutables.packedAddresses.taker(), bob.addr);
         assertEq(returnedImmutables.packedAddresses.token(), address(usdc));
         assertEq(returnedImmutables.timelocks.srcWithdrawalStart(), block.timestamp + srcTimelocks.finality);
-        assertEq(returnedImmutables.timelocks.srcCancellationStart(), block.timestamp + srcTimelocks.withdrawal);
-        assertEq(returnedImmutables.timelocks.srcPubCancellationStart(), block.timestamp + srcTimelocks.cancel);
+        assertEq(returnedImmutables.timelocks.srcCancellationStart(), block.timestamp + srcTimelocks.finality + srcTimelocks.withdrawal);
+        assertEq(returnedImmutables.timelocks.srcPubCancellationStart(), block.timestamp + srcTimelocks.finality + srcTimelocks.withdrawal + srcTimelocks.cancel);
         assertEq(returnedImmutables.timelocks.dstWithdrawalStart(), block.timestamp + dstTimelocks.finality);
-        assertEq(returnedImmutables.timelocks.dstPubWithdrawalStart(), block.timestamp + dstTimelocks.withdrawal);
-        assertEq(returnedImmutables.timelocks.dstCancellationStart(), block.timestamp + dstTimelocks.publicWithdrawal);
+        assertEq(returnedImmutables.timelocks.dstPubWithdrawalStart(), block.timestamp + dstTimelocks.finality + dstTimelocks.withdrawal);
+        assertEq(returnedImmutables.timelocks.dstCancellationStart(), block.timestamp + dstTimelocks.finality + dstTimelocks.withdrawal + dstTimelocks.publicWithdrawal);
     }
 
     function testFuzz_DeployCloneForTaker(bytes32 secret, uint56 amount) public {
@@ -91,11 +90,9 @@ contract EscrowFactoryTest is BaseSetup {
         assertEq(returnedImmutables.packedAddresses.maker(), alice.addr);
         assertEq(returnedImmutables.packedAddresses.taker(), bob.addr);
         assertEq(returnedImmutables.packedAddresses.token(), address(dai));
-        console.log(returnedImmutables.timelocks.dstWithdrawalStart());
-        console.log(dstTimelocks.finality);
-        // assertEq(returnedImmutables.timelocks.dstWithdrawalStart(), block.timestamp + dstTimelocks.finality);
-        // assertEq(returnedImmutables.timelocks.dstPubWithdrawalStart(), block.timestamp + dstTimelocks.withdrawal);
-        // assertEq(returnedImmutables.timelocks.dstCancellationStart(), block.timestamp + dstTimelocks.publicWithdrawal);
+        assertEq(returnedImmutables.timelocks.dstWithdrawalStart(), block.timestamp + dstTimelocks.finality);
+        assertEq(returnedImmutables.timelocks.dstPubWithdrawalStart(), block.timestamp + dstTimelocks.finality + dstTimelocks.withdrawal);
+        assertEq(returnedImmutables.timelocks.dstCancellationStart(), block.timestamp + dstTimelocks.finality + dstTimelocks.withdrawal + dstTimelocks.publicWithdrawal);
     }
 
     function test_NoInsufficientBalanceNativeDeploymentForMaker() public {
