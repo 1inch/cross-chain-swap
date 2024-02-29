7885fdbe9fc4806a8e483f35768ea8ca592ba450
Anton Bukov <k06aaa@gmail.com>
2024-02-29 16:13:00 +0100

    Simplify bytecode hash computation

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -26,13 +26,11 @@ abstract contract Escrow is Clone, IEscrow {
         RESCUE_DELAY = rescueDelay;
 
         bytes32 bytecodeHash;
-
         assembly ("memory-safe") {
-            let ptr := mload(0x40)
-            mstore(add(ptr, 0x24), 0x5af43d82803e903d91602b57fd5bf3ff)
-            mstore(add(ptr, 0x14), address())
-            mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73)
-            bytecodeHash := keccak256(add(ptr, 0x0c), 0x37)
+            mstore(0, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+            mstore(20, shl(96, address()))
+            mstore(40, 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+            bytecodeHash := keccak256(0, 55)
         }
         proxyBytecodeHash = bytecodeHash;
     }
