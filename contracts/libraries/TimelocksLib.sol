83b065b5dc6d096a15823ea1b8eb62f979ba1f56
byshape <xenia.shape@gmail.com>
2024-03-26 14:50:27 +0000

    Added comment about 2106

--- contracts/libraries/TimelocksLib.sol
+++ contracts/libraries/TimelocksLib.sol
@@ -69,6 +69,7 @@ library TimelocksLib {
     function get(Timelocks timelocks, Stage stage) internal pure returns (uint256) {
         uint256 data = Timelocks.unwrap(timelocks);
         uint256 bitShift = uint256(stage) * 32;
+        // The maximum uint32 value will be reached in 2106. 
         return uint32(data) + uint32(data >> bitShift);
     }
 }
