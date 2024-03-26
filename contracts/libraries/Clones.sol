adb654f5e79f573c5826d38fb51d8085827a796e
byshape <xenia.shape@gmail.com>
2024-03-26 14:39:21 +0000

    Fixed unsafe memory writing

--- contracts/libraries/Clones.sol
+++ contracts/libraries/Clones.sol
@@ -47,9 +47,9 @@ library Clones {
      */
     function computeProxyBytecodeHash(address implementation) internal pure returns (bytes32 bytecodeHash) {
         assembly ("memory-safe") {
-            mstore(0, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-            mstore(20, shl(96, implementation))
-            mstore(40, 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+            mstore(0, 0x3d602d80600a3d39000000000000000000000000000000000000000000000000)
+            mstore(23, 0x5af43d82803e903d91602b57fd5bf3)
+            mstore(8, or(0x81f3363d3d373d3d3d363d730000000000000000000000000000000000000000, implementation))
             bytecodeHash := keccak256(0, 55)
         }
     }
