0337d9319048f9e74224555d08256538593d2eb9
byshape <xenia.shape@gmail.com>
2024-04-03 17:17:24 +0100

    Updated `Clones.computeProxyBytecodeHash`

--- contracts/libraries/Clones.sol
+++ contracts/libraries/Clones.sol
@@ -47,10 +47,12 @@ library Clones {
      */
     function computeProxyBytecodeHash(address implementation) internal pure returns (bytes32 bytecodeHash) {
         assembly ("memory-safe") {
-            mstore(0, 0x3d602d80600a3d39000000000000000000000000000000000000000000000000)
-            mstore(23, 0x5af43d82803e903d91602b57fd5bf3)
-            mstore(8, or(0x81f3363d3d373d3d3d363d730000000000000000000000000000000000000000, implementation))
-            bytecodeHash := keccak256(0, 55)
+            mstore(0x20, 0x5af43d82803e903d91602b57fd5bf3)
+            // implementation address
+            mstore(0x11, implementation)
+            // Packs the first 3 bytes of the `implementation` address with the bytecode before the address.
+            mstore(0x00, or(shr(0x88, implementation), 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000))
+            bytecodeHash := keccak256(0x09, 0x37)
         }
     }
 }
