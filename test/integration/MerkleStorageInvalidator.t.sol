30cb32c8863b5e68613d5f42e3dff546fadc0af7
byshape <xenia.shape@gmail.com>
2024-05-17 15:02:03 +0100

    Add tests for `calculatedIndex`

--- test/integration/MerkleStorageInvalidator.t.sol
+++ test/integration/MerkleStorageInvalidator.t.sol
@@ -314,5 +314,380 @@ contract MerkleStorageInvalidatorIntTest is BaseSetup {
         );
     }
 
+    function test_MultipleFillsFillFirst() public {
+        uint256 idx = 0;
+        uint256 makingAmount = MAKING_AMOUNT / SECRETS_AMOUNT;
+        bytes32[] memory proof = merkle.getProof(hashedPairs, idx);
+        assert(merkle.verifyProof(root, proof, hashedPairs[idx]));
+
+        bytes32 rootPlusAmount = bytes32(SECRETS_AMOUNT << 240 | uint240(uint256(root)));
+
+        (
+            IOrderMixin.Order memory order,
+            bytes32 orderHash,
+            /* bytes memory extraData */,
+            bytes memory extension,
+            IEscrow srcClone,
+            IEscrow.Immutables memory immutables
+        ) = _prepareDataSrc(rootPlusAmount, MAKING_AMOUNT, TAKING_AMOUNT, SRC_SAFETY_DEPOSIT, DST_SAFETY_DEPOSIT, address(0), false, true);
+
+        immutables.hashlock = hashedSecrets[idx];
+        immutables.amount = makingAmount;
+        srcClone = EscrowSrc(escrowFactory.addressOfEscrowSrc(immutables));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alice, orderHash);
+        bytes32 vs = bytes32((uint256(v - 27) << 255)) | s;
+
+        bytes memory interaction = abi.encodePacked(escrowFactory, abi.encode(root, proof, idx, hashedSecrets[idx]));
+
+        (TakerTraits takerTraits, bytes memory args) = _buildTakerTraits(
+            true, // makingAmount
+            false, // unwrapWeth
+            false, // skipMakerPermit
+            false, // usePermit2
+            address(srcClone), // target
+            extension,
+            interaction,
+            0 // threshold
+        );
+
+        (bool success,) = address(srcClone).call{ value: SRC_SAFETY_DEPOSIT }("");
+        assertEq(success, true);
+
+        uint256 resolverCredit = feeBank.availableCredit(bob.addr);
+
+        vm.prank(bob.addr);
+        limitOrderProtocol.fillOrderArgs(
+            order,
+            r,
+            vs,
+            makingAmount, // amount
+            takerTraits,
+            args
+        );
+
+        assertLt(feeBank.availableCredit(bob.addr), resolverCredit);
+        assertEq(usdc.balanceOf(address(srcClone)), makingAmount);
+    }
+
+    function test_MultipleFillsFillFirstTwoFills() public {
+        uint256 idx = 0;
+        uint256 makingAmount = MAKING_AMOUNT / SECRETS_AMOUNT / 2;
+        bytes32[] memory proof = merkle.getProof(hashedPairs, idx);
+        assert(merkle.verifyProof(root, proof, hashedPairs[idx]));
+
+        bytes32 rootPlusAmount = bytes32(SECRETS_AMOUNT << 240 | uint240(uint256(root)));
+
+        (
+            IOrderMixin.Order memory order,
+            bytes32 orderHash,
+            /* bytes memory extraData */,
+            bytes memory extension,
+            IEscrow srcClone,
+            IEscrow.Immutables memory immutables
+        ) = _prepareDataSrc(rootPlusAmount, MAKING_AMOUNT, TAKING_AMOUNT, SRC_SAFETY_DEPOSIT, DST_SAFETY_DEPOSIT, address(0), false, true);
+
+        immutables.hashlock = hashedSecrets[idx];
+        immutables.amount = makingAmount;
+        srcClone = EscrowSrc(escrowFactory.addressOfEscrowSrc(immutables));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alice, orderHash);
+        bytes32 vs = bytes32((uint256(v - 27) << 255)) | s;
+
+        bytes memory interaction = abi.encodePacked(escrowFactory, abi.encode(root, proof, idx, hashedSecrets[idx]));
+
+        (TakerTraits takerTraits, bytes memory args) = _buildTakerTraits(
+            true, // makingAmount
+            false, // unwrapWeth
+            false, // skipMakerPermit
+            false, // usePermit2
+            address(srcClone), // target
+            extension,
+            interaction,
+            0 // threshold
+        );
+
+        (bool success,) = address(srcClone).call{ value: SRC_SAFETY_DEPOSIT }("");
+        assertEq(success, true);
+
+        uint256 resolverCredit = feeBank.availableCredit(bob.addr);
+
+        vm.prank(bob.addr);
+        limitOrderProtocol.fillOrderArgs(
+            order,
+            r,
+            vs,
+            makingAmount, // amount
+            takerTraits,
+            args
+        );
+
+        assertLt(feeBank.availableCredit(bob.addr), resolverCredit);
+        assertEq(usdc.balanceOf(address(srcClone)), makingAmount);
+
+        // ------------ 2nd fill ------------ //
+        idx = 1;
+        uint256 makingAmount2 = MAKING_AMOUNT / SECRETS_AMOUNT * 3 / 2; // Fill half of the 0-th and  full of the 1-st
+        proof = merkle.getProof(hashedPairs, idx);
+        assert(merkle.verifyProof(root, proof, hashedPairs[idx]));
+
+        immutables.hashlock = hashedSecrets[idx];
+        immutables.amount = makingAmount2;
+        address srcClone2 = address(EscrowSrc(escrowFactory.addressOfEscrowSrc(immutables)));
+
+        (v, r, s) = vm.sign(alice, orderHash);
+        vs = bytes32((uint256(v - 27) << 255)) | s;
+
+        interaction = abi.encodePacked(escrowFactory, abi.encode(root, proof, idx, hashedSecrets[idx]));
+
+        (TakerTraits takerTraits2, bytes memory args2) = _buildTakerTraits(
+            true, // makingAmount
+            false, // unwrapWeth
+            false, // skipMakerPermit
+            false, // usePermit2
+            srcClone2, // target
+            extension,
+            interaction,
+            0 // threshold
+        );
+
+        (success,) = srcClone2.call{ value: SRC_SAFETY_DEPOSIT }("");
+        assertEq(success, true);
+
+        vm.prank(bob.addr);
+        limitOrderProtocol.fillOrderArgs(
+            order,
+            r,
+            vs,
+            makingAmount2, // amount
+            takerTraits2,
+            args2
+        );
+
+        assertEq(usdc.balanceOf(address(srcClone2)), makingAmount2);
+        (uint256 storedIndex,) = IMerkleStorageInvalidator(escrowFactory).lastValidated(
+            keccak256(abi.encodePacked(orderHash, uint240(uint256(root))))
+        );
+        assertEq(storedIndex, idx + 1);
+    }
+
+    function test_MultipleFillsFillLast() public {
+        uint256 idx = SECRETS_AMOUNT - 1;
+        bytes32[] memory proof = merkle.getProof(hashedPairs, idx);
+        assert(merkle.verifyProof(root, proof, hashedPairs[idx]));
+
+        bytes32 rootPlusAmount = bytes32(SECRETS_AMOUNT << 240 | uint240(uint256(root)));
+
+        (
+            IOrderMixin.Order memory order,
+            bytes32 orderHash,
+            /* bytes memory extraData */,
+            bytes memory extension,
+            IEscrow srcClone,
+            IEscrow.Immutables memory immutables
+        ) = _prepareDataSrc(rootPlusAmount, MAKING_AMOUNT, TAKING_AMOUNT, SRC_SAFETY_DEPOSIT, DST_SAFETY_DEPOSIT, address(0), false, true);
+
+        immutables.hashlock = hashedSecrets[idx];
+        srcClone = EscrowSrc(escrowFactory.addressOfEscrowSrc(immutables));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alice, orderHash);
+        bytes32 vs = bytes32((uint256(v - 27) << 255)) | s;
+
+        bytes memory interaction = abi.encodePacked(escrowFactory, abi.encode(root, proof, idx, hashedSecrets[idx]));
+
+        (TakerTraits takerTraits, bytes memory args) = _buildTakerTraits(
+            true, // makingAmount
+            false, // unwrapWeth
+            false, // skipMakerPermit
+            false, // usePermit2
+            address(srcClone), // target
+            extension,
+            interaction,
+            0 // threshold
+        );
+
+        (bool success,) = address(srcClone).call{ value: SRC_SAFETY_DEPOSIT }("");
+        assertEq(success, true);
+
+        uint256 resolverCredit = feeBank.availableCredit(bob.addr);
+
+        vm.prank(bob.addr);
+        limitOrderProtocol.fillOrderArgs(
+            order,
+            r,
+            vs,
+            MAKING_AMOUNT, // amount
+            takerTraits,
+            args
+        );
+
+        assertLt(feeBank.availableCredit(bob.addr), resolverCredit);
+        assertEq(usdc.balanceOf(address(srcClone)), MAKING_AMOUNT);
+    }
+
+    function test_MultipleFillsFillAllTwoFills() public {
+        uint256 idx = SECRETS_AMOUNT - 2;
+        uint256 makingAmount = MAKING_AMOUNT * (idx + 1) / SECRETS_AMOUNT;
+        bytes32[] memory proof = merkle.getProof(hashedPairs, idx);
+        assert(merkle.verifyProof(root, proof, hashedPairs[idx]));
+
+        bytes32 rootPlusAmount = bytes32(SECRETS_AMOUNT << 240 | uint240(uint256(root)));
+
+        (
+            IOrderMixin.Order memory order,
+            bytes32 orderHash,
+            /* bytes memory extraData */,
+            bytes memory extension,
+            IEscrow srcClone,
+            IEscrow.Immutables memory immutables
+        ) = _prepareDataSrc(rootPlusAmount, MAKING_AMOUNT, TAKING_AMOUNT, SRC_SAFETY_DEPOSIT, DST_SAFETY_DEPOSIT, address(0), false, true);
+
+        immutables.hashlock = hashedSecrets[idx];
+        immutables.amount = makingAmount;
+        srcClone = EscrowSrc(escrowFactory.addressOfEscrowSrc(immutables));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alice, orderHash);
+        bytes32 vs = bytes32((uint256(v - 27) << 255)) | s;
+
+        bytes memory interaction = abi.encodePacked(escrowFactory, abi.encode(root, proof, idx, hashedSecrets[idx]));
+
+        (TakerTraits takerTraits, bytes memory args) = _buildTakerTraits(
+            true, // makingAmount
+            false, // unwrapWeth
+            false, // skipMakerPermit
+            false, // usePermit2
+            address(srcClone), // target
+            extension,
+            interaction,
+            0 // threshold
+        );
+
+        (bool success,) = address(srcClone).call{ value: SRC_SAFETY_DEPOSIT }("");
+        assertEq(success, true);
+
+
+        vm.prank(bob.addr);
+        limitOrderProtocol.fillOrderArgs(
+            order,
+            r,
+            vs,
+            makingAmount, // amount
+            takerTraits,
+            args
+        );
+
+        assertEq(usdc.balanceOf(address(srcClone)), makingAmount);
+
+        // ------------ 2nd fill ------------ //
+        idx = SECRETS_AMOUNT - 1;
+        uint256 makingAmount2 = MAKING_AMOUNT - makingAmount;
+        proof = merkle.getProof(hashedPairs, idx);
+        assert(merkle.verifyProof(root, proof, hashedPairs[idx]));
+
+        immutables.hashlock = hashedSecrets[idx];
+        immutables.amount = makingAmount2;
+        address srcClone2 = address(EscrowSrc(escrowFactory.addressOfEscrowSrc(immutables)));
+
+        (v, r, s) = vm.sign(alice, orderHash);
+        vs = bytes32((uint256(v - 27) << 255)) | s;
+
+        interaction = abi.encodePacked(escrowFactory, abi.encode(root, proof, idx, hashedSecrets[idx]));
+
+        (TakerTraits takerTraits2, bytes memory args2) = _buildTakerTraits(
+            true, // makingAmount
+            false, // unwrapWeth
+            false, // skipMakerPermit
+            false, // usePermit2
+            srcClone2, // target
+            extension,
+            interaction,
+            0 // threshold
+        );
+
+        (success,) = srcClone2.call{ value: SRC_SAFETY_DEPOSIT }("");
+        assertEq(success, true);
+
+        vm.prank(bob.addr);
+        limitOrderProtocol.fillOrderArgs(
+            order,
+            r,
+            vs,
+            makingAmount2, // amount
+            takerTraits2,
+            args2
+        );
+
+        assertEq(usdc.balanceOf(address(srcClone2)), makingAmount2);
+        (uint256 storedIndex,) = IMerkleStorageInvalidator(escrowFactory).lastValidated(
+            keccak256(abi.encodePacked(orderHash, uint240(uint256(root))))
+        );
+        assertEq(storedIndex, SECRETS_AMOUNT);
+    }
+
+    function test_MultipleFillsOddDivision() public {
+        uint256 secretsAmount = 8;
+        uint256 makingAmount = 135;
+        uint256 makingAmountToFill = makingAmount / 2;
+        uint256 idx = secretsAmount * (makingAmountToFill - 1) / makingAmount;
+        bytes32[] memory hashedS = new bytes32[](secretsAmount);
+        bytes32[] memory hashedP = new bytes32[](secretsAmount);
+        for (uint256 i = 0; i < secretsAmount; i++) {
+            hashedS[i] = keccak256(abi.encodePacked(i));
+            hashedP[i] = keccak256(abi.encodePacked(i, hashedS[i]));
+        }
+        root = merkle.getRoot(hashedP);
+        bytes32[] memory proof = merkle.getProof(hashedP, idx);
+        assert(merkle.verifyProof(root, proof, hashedP[idx]));
+
+        bytes32 rootPlusAmount = bytes32(secretsAmount << 240 | uint240(uint256(root)));
+
+        (
+            IOrderMixin.Order memory order,
+            bytes32 orderHash,
+            /* bytes memory extraData */,
+            bytes memory extension,
+            IEscrow srcClone,
+            IEscrow.Immutables memory immutables
+        ) = _prepareDataSrc(rootPlusAmount, makingAmount, TAKING_AMOUNT, SRC_SAFETY_DEPOSIT, DST_SAFETY_DEPOSIT, address(0), false, true);
+
+        immutables.hashlock = hashedS[idx];
+        immutables.amount = makingAmountToFill;
+        srcClone = EscrowSrc(escrowFactory.addressOfEscrowSrc(immutables));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alice, orderHash);
+        bytes32 vs = bytes32((uint256(v - 27) << 255)) | s;
+
+        bytes memory interaction = abi.encodePacked(escrowFactory, abi.encode(root, proof, idx, hashedS[idx]));
+
+        (TakerTraits takerTraits, bytes memory args) = _buildTakerTraits(
+            true, // makingAmount
+            false, // unwrapWeth
+            false, // skipMakerPermit
+            false, // usePermit2
+            address(srcClone), // target
+            extension,
+            interaction,
+            0 // threshold
+        );
+
+        (bool success,) = address(srcClone).call{ value: SRC_SAFETY_DEPOSIT }("");
+        assertEq(success, true);
+
+        uint256 resolverCredit = feeBank.availableCredit(bob.addr);
+
+        vm.prank(bob.addr);
+        limitOrderProtocol.fillOrderArgs(
+            order,
+            r,
+            vs,
+            makingAmountToFill, // amount
+            takerTraits,
+            args
+        );
+
+        assertLt(feeBank.availableCredit(bob.addr), resolverCredit);
+        assertEq(usdc.balanceOf(address(srcClone)), makingAmountToFill);
+    }
+
     /* solhint-enable func-name-mixedcase */
 }
