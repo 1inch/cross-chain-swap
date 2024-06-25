9779e669f9512b630ea5e9edeed22df5162aab8e
Xenia <94478708+byshape@users.noreply.github.com>
2024-06-25 11:55:45 +0100

    Apply suggestions from code review
    Co-authored-by: Mikhail Melnik <by.zumzoom@gmail.com>

--- contracts/zkSync/EscrowDstZkSync.sol
+++ contracts/zkSync/EscrowDstZkSync.sol
@@ -6,7 +6,7 @@ import { Escrow, EscrowDst } from "contracts/EscrowDst.sol";
 import { EscrowZkSync } from "contracts/zkSync/EscrowZkSync.sol";
 
 contract EscrowDstZkSync is EscrowDst, EscrowZkSync {
-    constructor(uint32 rescueDelay) EscrowDst(rescueDelay) EscrowZkSync() payable {}
+    constructor(uint32 rescueDelay) EscrowDst(rescueDelay) EscrowZkSync() {}
 
     function _validateImmutables(Immutables calldata immutables) internal view override(Escrow, EscrowZkSync) {
         EscrowZkSync._validateImmutables(immutables);
