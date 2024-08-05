76fe89c603dba3cffafb9bea1f711ef6f0d40639
byshape <xenia.shape@gmail.com>
2024-08-05 19:21:43 +0100

    Add test for reentrancy

--- test/integration/EscrowFactory.t.sol
+++ test/integration/EscrowFactory.t.sol
@@ -2,12 +2,14 @@
 pragma solidity 0.8.23;
 
 import { TakerTraits } from "limit-order-protocol/contracts/libraries/TakerTraitsLib.sol";
+import { Merkle } from "murky/src/Merkle.sol";
 import { Address } from "solidity-utils/contracts/libraries/AddressLib.sol";
 
 import { IEscrowFactory } from "contracts/interfaces/IEscrowFactory.sol";
 
 import { BaseSetup } from "../utils/BaseSetup.sol";
 import { CrossChainTestLib } from "../utils/libraries/CrossChainTestLib.sol";
+import { ResolverReentrancy } from "../utils/mocks/ResolverReentrancy.sol";
 
 contract IntegrationEscrowFactoryTest is BaseSetup {
     function setUp() public virtual override {
@@ -143,6 +145,66 @@ contract IntegrationEscrowFactoryTest is BaseSetup {
             r,
             vs,
             MAKING_AMOUNT, // amount
+            takerTraits,
+            args
+        );
+    }
+
+    function test_NoResolverReentrancy() public {
+        ResolverReentrancy badResolver = new ResolverReentrancy(escrowFactory, limitOrderProtocol, address(this)); 
+        resolvers[0] = address(badResolver);
+        vm.deal(address(badResolver), 100 ether);
+
+        uint256 partsAmount = 100;
+        uint256 secretsAmount = partsAmount + 1;
+        bytes32[] memory hashedSecrets = new bytes32[](secretsAmount);
+        bytes32[] memory hashedPairs = new bytes32[](secretsAmount);
+        for (uint64 i = 0; i < secretsAmount; i++) {
+            // Note: This is not production-ready code. Use cryptographically secure random to generate secrets.
+            hashedSecrets[i] = keccak256(abi.encodePacked(i));
+            hashedPairs[i] = keccak256(abi.encodePacked(i, hashedSecrets[i]));
+        }
+        Merkle merkle = new Merkle();
+        bytes32 root = merkle.getRoot(hashedPairs);
+        bytes32 rootPlusAmount = bytes32(partsAmount << 240 | uint240(uint256(root)));
+        uint256 idx = 0;
+        uint256 makingAmount = MAKING_AMOUNT / partsAmount;
+        bytes32[] memory proof = merkle.getProof(hashedPairs, idx);
+        assert(merkle.verifyProof(root, proof, hashedPairs[idx]));
+
+        vm.warp(1710288000); // set current timestamp
+        (timelocks, timelocksDst) = CrossChainTestLib.setTimelocks(srcTimelocks, dstTimelocks);
+
+
+        CrossChainTestLib.SwapData memory swapData = _prepareDataSrcHashlock(rootPlusAmount, false, true);
+
+        swapData.immutables.hashlock = hashedSecrets[idx];
+        swapData.immutables.amount = makingAmount - 2;
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(alice.privateKey, swapData.orderHash);
+        bytes32 vs = bytes32((uint256(v - 27) << 255)) | s;
+
+        bytes memory interaction = abi.encodePacked(address(badResolver));
+        bytes memory interactionFull = abi.encodePacked(interaction, escrowFactory, abi.encode(proof, idx, hashedSecrets[idx]));
+
+        (TakerTraits takerTraits, bytes memory args) = CrossChainTestLib.buildTakerTraits(
+            true, // makingAmount
+            false, // unwrapWeth
+            false, // skipMakerPermit
+            false, // usePermit2
+            address(0), // target
+            swapData.extension, // extension
+            interactionFull,
+            0 // threshold
+        );
+
+        vm.expectRevert(IEscrowFactory.InvalidPartialFill.selector);
+        badResolver.deploySrc(
+            swapData.immutables,
+            swapData.order,
+            r,
+            vs,
+            makingAmount - 2,
             takerTraits,
             args
         );
